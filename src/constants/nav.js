61c232372b85acfd7467225d3429e27059598002
Tazminur Rahman <m2air@m2s-MacBook-Air.local>
2026-01-16 11:20:52 +0600

    Icon chnage

--- src/constants/nav.js
+++ src/constants/nav.js
@@ -136,7 +136,7 @@ export const navigation = [
         children: [
           { name: 'Haji List', href: '/hajj-umrah/haji-list', icon: List },
           { name: 'Add New Haji', href: '/hajj-umrah/haji/add', icon: Plus },
-          { name: 'HL Manage', href: '/hajj-umrah/license-management', icon: Eye },
+          { name: 'HL Manage', href: '/hajj-umrah/license-management', icon: Shield },
           { name: 'Haj Package', href: '/hajj-umrah/haj-package-list', icon: List }
         ]
       },
